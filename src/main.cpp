65c4aea78bdb419dd6d08806990056bb72fc0cd0
Iceflower <iceflower@gmx.de>
2016-03-02 17:01:47 +0100

    Big Update
    Add
    - support for running with arguments
    - costum import/ export directories (not completly finished)
    - creating multiple directories
    
    Update
    - code cleanup
    - Todo

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 /* TODO:
-    use "import" as import file
-    Future:
-        custom files
+    progressbar for import reading
+    handle different partitions under windows with absolut paths
+    access vor MVS support
+    documentation
+    -help support
 */
 
 #if defined __GNUC__
@@ -14,13 +16,12 @@
     #include <io.h>
 #endif
 
+#include <errno.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string.h>
 
-#include <iomanip>
-#include <stdint.h>
 #include <thread>
 
 #include <time.h>
@@ -32,27 +33,103 @@
 #endif
 #include <cmath>
 #include <vector>
+#include <iomanip>
+#include <stdint.h>
 
-    bool readInputFile(const std::string path, std::vector<float>& dataList);
-    void DFT(const std::vector<float>& dataList, const unsigned int start, const unsigned int ends, std::vector<float>& threadOutputList);
-    void calcPartSize(std::vector<unsigned int>& boundsList, const unsigned int numbers, const unsigned int& cores);
-    void DFTprogress(const std::vector< std::vector<float> >& outputList, const unsigned int finishValue, const int updateTime, const int showBarTheme);
-    bool saveToFile(const std::vector< std::vector<float> >& outputList, const std::string DFToutputPath);
-    const std::string getProgressBar(float percent);
-    const std::string getCurrentTime();
+void getPaths(std::string &sourcePath, std::string &exportPath);
+void correctDirPath(std::string& path);
+void correctFilePath(std::string& path);
+const std::string checkFileAccess(std::string path, int arg);
+const bool createDir(std::string path);
+bool readInputFile(const std::string path, std::vector<float>& dataList);
+void calcPartSize(std::vector<unsigned int>& boundsList, const unsigned int numbers, const unsigned int& cores);
+void DFT(const std::vector<float>& dataList, const unsigned int start, const unsigned int ends, std::vector<float>& threadOutputList);
+const std::string getProgressBar(float percent);
+void DFTprogress(const std::vector< std::vector<float> >& outputList, const unsigned int finishValue, const int updateTime, const int showBarTheme);
+const std::string getCurrentTime();
+bool saveToFile(const std::vector< std::vector<float> >& outputList, const std::string DFToutputPath);
 
-int main() {
+
+int main(int argc, char* argv[]) {
     std::cout << "Direct Fourier Transformation of 3D points. - under development" << std::endl << std::endl;
+    std::cout << "Start the program with the argument -help for more information about starting with arguments." << std::endl;
     std::cout << "WARNING: This program will try to use all cores! Your system should run stable though." << std::endl;
-    std::cout << "The source data has to be named as \"source.pos\" and placed in \"<DFT program file>/import\"" << std::endl;
     std::cout << std::endl;
 
+    //get paths
+    std::string sourcePath;
+    std::string exportPath;
+    bool forceCreateExpPath = false; //force creating export path
+    {
+        //path managing
+        if (argc == 1) { //if no paths are given with the arguments
+            getPaths(std::ref(sourcePath), std::ref(exportPath)); //ask for import and export path
+        } else {
+            sourcePath = argv[1]; //source path is the first argument
+            if (argc > 2) { //export path is second argument
+                exportPath = argv[2];
+            } else {
+                exportPath = "export/";
+            }
+            if (argc > 3) { //force creating export path is third argument
+                if (strcmp(argv[3], "0") != 0 && strcmp(argv[3], "1") != 0) { //check if the argument is correct
+                    std::cout << "ERROR: wrong third argument" << std::endl; //status msg
+                    std::cout << "CLOSED" << std::endl;
+                    return -1;
+                }
+                forceCreateExpPath = (strcmp(argv[3], "1") == 0);
+            }
+        }
+
+        //correct some mistakes in the paths
+        correctFilePath(std::ref(sourcePath));
+        correctDirPath(std::ref(exportPath));
+
+        //check paths
+        std::string tmpmsg = checkFileAccess(sourcePath, 1);
+        if (tmpmsg.compare("") != 0) { //check source file path
+            std::cout << "ERROR: source file is " << tmpmsg << std::endl; //status msg
+            std::cout << "CLOSED" << std::endl;
+            return -1;
+        }
+
+        if ((access(exportPath.c_str(), F_OK) != 0) && (errno == ENOENT)) { //check existing of export path file
+            std::cout << "NOTE: export path is not existing." << std::endl;
+            if (!forceCreateExpPath) { //no forcing creation of export path
+                std::cout << "Should file created? [y/n]" << std::endl;
+                char tmpchar;
+                std::cin >> tmpchar;
+                std::cin.get();
+                if (tmpchar != 'y') {
+                    std::cout << "User canceled." << std::endl;
+                    std::cout << "CLOSED" << std::endl;
+                    return 1;
+                }
+            }
+            std::cout << "START: Creating export directory" << std::endl; //status msg
+            if (!createDir(exportPath)) { //create directory
+                std::cout << "ERROR: Creating export directory" << std::endl; //status msg
+                std::cout << "CLOSED" << std::endl;
+                return -1;
+            }
+            std::cout << "DONE: Creating export directory" << std::endl; //status msg
+        } else { //is existing but has another error
+            tmpmsg = checkFileAccess(exportPath, 2);
+            if (tmpmsg.compare("") != 0) {
+                std::cout << "ERROR: export path is " << tmpmsg << std::endl; //status msg
+                std::cout << "CLOSED" << std::endl;
+                return -1;
+            }
+        }
+    }
+
+    //import source
     std::vector<float> dataList; //0: x Coord.; 1: y Coord.; 2: z Coord.
     std::cout << "START: import data" << std::endl; //status msg
     std::cout << getProgressBar(-2) << std::endl; //show the snail; until theres now progressbar for input reading
-    if (!readInputFile("import/source.pos", std::ref(dataList))) { //read data from file and save it into dataList
+    if (!readInputFile(sourcePath, std::ref(dataList))) { //read data from file and save it into dataList
         std::cout << "CLOSED" << std::endl; //status msg
-        return 0;
+        return 1;
     }
     std::cout << "DONE: import data" << std::endl; //status msg
 
@@ -102,7 +179,8 @@ int main() {
         outputList.push_back(std::vector<float>());
     }
 
-    { //DFT
+    {
+        //DFT
         std::vector<unsigned int> boundsList; //init the bounds vector
         calcPartSize(boundsList, (dataList.size() / 3), maxThreads); //save the bounds into boundsList
 
@@ -130,32 +208,151 @@ int main() {
     } //end - DFT
 
     std::cout << "START: saving DFT data" << std::endl; //status msg
-    if (!saveToFile(std::ref(outputList), "export/")) { //save DFT results to file
+    if (!saveToFile(std::ref(outputList), exportPath)) { //save DFT results to file
         std::cout << "CLOSED" << std::endl; //status msg
-        return 0;
+        return -1;
     }
     std::cout << "DONE: saving DFT data" << std::endl; //status msg
-    std::cout << "Press any key to close the terminal.";
-    getchar();
+    //if (argc == 1) { ///DEV //does not need to close it manually if arguments were given, because automatic systems doesnt need those
+        std::cout << "Press return to close the program.";
+        std::cin.get();
+    //}
     return 0;
+}
+
+void getPaths(std::string &sourcePath, std::string &exportPath) {
+    std::cout << "NOTE: no arguments given" << std::endl; //status msg
+    std::cout << "\tYou can use absolute paths or a relative path based on the execution file" << std::endl;
+    std::cout << "\tUse /" << std::endl;
+    std::cout << "Source file: ";
+    std::getline(std::cin, sourcePath);
+    std::cout << std::endl;
+
+    std::cout << "\tIf you dont set a export path the export folder in your execution file will be used." << std::endl;
+    std::cout << "Export path: ";
+    std::getline(std::cin, exportPath);
+    if (exportPath.compare("") == 0) {
+        exportPath = "export/";
+    }
+    std::cout << std::endl;
+}
+
+void correctDirPath(std::string& path) {
+    size_t pos = path.find("\\");
+    while(pos != std::string::npos) {
+        path.replace(pos, 1, "/");
+        pos = path.find("\\");
+    }
+    pos = path.find("//");
+    while(pos != std::string::npos) {
+        path.replace(pos, 2, "/");
+        pos = path.find("//");
+    }
+    if (path[0] == '/') {
+        path.erase(0,1);
+    }
+    if (path[path.size() - 1] != '/') {
+        path += '/';
+    }
+}
+
+void correctFilePath(std::string& path) {
+    size_t pos = path.find("\\");
+    while(pos != std::string::npos) {
+        path.replace(pos, 1, "/");
+        pos = path.find("\\");
+    }
+    pos = path.find("//");
+    while(pos != std::string::npos) {
+        path.replace(pos, 2, "/");
+        pos = path.find("//");
+    }
+    if (path[0] == '/') {
+        path.erase(0,1);
+    }
+}
+
+/*
+    #if defined __GNUC__
+    if(access(DFToutputPath.c_str(), F_OK | W_OK ) == -1 ) { //if file is not existing
+    #elif defined _MSC_VER
+    if(access(DFToutputPath.c_str(), 0) == -1) { //if file is not existing
+    #endif
+*/
+
+const std::string checkFileAccess(std::string path, int arg) { //arg = 1: read access; arg = 2: write access; arg = 3: read & write access
+    int accres;
+
+    //check exist
+    accres = access(path.c_str(), F_OK);
+    if (accres != 0) {
+        if (errno == ENOENT) {
+            return "not existing.";
+        } else if (errno == EACCES) {
+            return "not accessible.";
+        } else {
+            return "not accessible.";
+        }
+    }
+
+    //check read
+    if (arg & 1) {
+        accres = access(path.c_str(), R_OK);
+        if (accres != 0) {
+            return "not readable (access denied).";
+        }
+    }
+
+    //check write
+    if (arg & 1) {
+        accres = access(path.c_str(), W_OK);
+        if (accres != 0) {
+            if (errno == EACCES) {
+                return "not writable (access denied).";
+            } else if (errno == EROFS) {
+                return "not writable (read-only filesystem).";
+            } else {
+                return "not writable.";
+            }
+        }
+    }
+    return "";
+}
+
+const bool createDir(std::string path) {
+    std::string curPath = path;
+    size_t pos = path.find("/");;
+    curPath = path.substr(0, pos + 1);
+    while (pos != std::string::npos) {
+        #if defined _linux
+        if (mkdir(curPath.c_str(), 0) != 0) {
+        #elif defined _WIN32 || _WIN64
+        if (mkdir(curPath.c_str()) != 0) {
+        #endif
+            if (errno != EEXIST) {
+                return false;
+            }
+        }
+        pos = path.find("/", pos + 1);
+        curPath = path.substr(0, pos + 1);
+    }
+    return true;
 }
 
 bool readInputFile(const std::string path, std::vector<float>& dataList) { ///NOTE: changes dataList
     std::ifstream inputFile;
     uint32_t number;
     float floatnum;
+
     inputFile.open(path, std::ios::in | std::ios::binary);
-    if (!inputFile.good()) {
+
+    std::string tmpmsg = checkFileAccess(path, 1);
+    if (tmpmsg.compare("") != 0) {
         std::cout << "ERROR: import data" << std::endl; //status msg
-        std::cout << "\tSource file can not open." << std::endl; //status msg
+        std::cout << "\timport data is " << tmpmsg << std::endl; //status msg
         return false;
     }
     for (unsigned int i = 0; inputFile.read((char*)&number, sizeof(float)); i++) { //instead of i++ maybe (i % 4) (but slower)
-        if (!inputFile.good()) {
-            std::cout << "ERROR: import data" << std::endl; //status msg
-            std::cout << "\tError while reading the source file." << std::endl; //status msg
-            return false;
-        }
         //convert little endian to big endian because the input is 32bit float big endian
         #if defined __GNUC__
             number = (__builtin_bswap32(number));
@@ -169,6 +366,11 @@ bool readInputFile(const std::string path, std::vector<float>& dataList) { ///NO
         if ((i % 4) != 3) { //dont save every 4th input number (its the mass)
             dataList.push_back(floatnum);
         }
+    }
+    if (!inputFile.good() && !inputFile.eof()) {
+        std::cout << "ERROR: import data" << std::endl; //status msg
+        std::cout << "\tError while reading the source file." << std::endl; //status msg
+        return false;
     }
     inputFile.close();
     return true;
@@ -221,6 +423,25 @@ void DFT(const std::vector<float>& dataList, const unsigned int start, const uns
     }
 }
 
+const std::string getProgressBar(const float percent) { //progressbar with numbers after the decimal point
+    std::stringstream progressBar;
+    if ((percent < 0) || (percent > 100)) {
+        return "                    ...  _o**  ...                   ..."; //if illegal percent value show a snail ;)
+    }
+    //create the "empty" part of the bar + percent view
+    progressBar.width((110 - (percent)) / 2 - ((percent == 100) ? 1 : 0) - ( ((((int) percent) == percent) && ((int) percent % 2 != 1) ? 1 : 0)));
+    progressBar.fill(' ');
+    progressBar << std::fixed << std::setprecision(((percent != 100) ? 1 : 0)) << percent << "%" << ((percent != 100) ? "" : " "); //last output fixes a doubled '%' at 100% cause of float output before
+    std::string secondPart = progressBar.str();
+    progressBar.str(std::string());
+    //create the "full" part of the bar
+    progressBar.width((percent + 4) / 2);
+    progressBar.fill('=');
+    progressBar << " ";
+    progressBar << secondPart; //smelt both parts to one
+    return progressBar.str();
+}
+
 void DFTprogress(const std::vector< std::vector<float> >& outputList, const unsigned int finishValue, const int updateTime, const int showBarTheme) { //showbarTheme: 0 = absolut; 1 = percentage; 2 = percentage + absolut; 3 = percentage + absolut + remaining time
     time_t start, ends;
     time(&start);
@@ -229,9 +450,9 @@ void DFTprogress(const std::vector< std::vector<float> >& outputList, const unsi
     unsigned int tmpCurRawProgress = 0;
     while(curRawProgress < finishValue) { //work until the finishValue (100%) is reached
         #if defined __GNUC__
-            sleep(updateTime); //in seconds
+        sleep(updateTime); //in seconds
         #elif defined _MSC_VER
-            Sleep(updateTime * 1000); //in seconds
+        Sleep(updateTime * 1000); //in seconds
         #endif
         tmpCurRawProgress = 0;
         for(auto curVec : outputList) {
@@ -260,39 +481,17 @@ void DFTprogress(const std::vector< std::vector<float> >& outputList, const unsi
     std::cout << std::endl;
 }
 
-const std::string getProgressBar(const float percent) { //progressbar with numbers after the decimal point
-    std::stringstream progressBar;
-    if ((percent < 0) || (percent > 100)) {
-        return "                    ...  _o**  ...                   ..."; //if illegal percent value show a snail ;)
-    }
-    //create the "empty" part of the bar + percent view
-    progressBar.width((110 - (percent)) / 2 - ((percent == 100) ? 1 : 0) - ( ((((int) percent) == percent) && ((int) percent % 2 != 1) ? 1 : 0)));
-    progressBar.fill(' ');
-    progressBar << std::fixed << std::setprecision(((percent != 100) ? 1 : 0)) << percent << "%" << ((percent != 100) ? "" : " "); //last output fixes a doubled '%' at 100% cause of float output before
-    std::string secondPart = progressBar.str();
-    progressBar.str(std::string());
-    //create the "full" part of the bar
-    progressBar.width((percent + 4) / 2);
-    progressBar.fill('=');
-    progressBar << " ";
-    progressBar << secondPart; //smelt both parts to one
-    return progressBar.str();
+const std::string getCurrentTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[25];
+    tstruct = *localtime(&now);
+    strftime(buf, sizeof(buf), "%Y_%m_%d-%H_%M_%S", &tstruct);
+    return buf;
 }
 
 bool saveToFile(const std::vector< std::vector<float> >& outputList, const std::string DFToutputPath) {
     std::string DFToutputFile;
-    #if defined __GNUC__
-        if(access(DFToutputPath.c_str(), F_OK ) == -1 ) { //if file is not existing
-    #elif defined _MSC_VER
-        if(access(DFToutputPath.c_str(), 0) == -1) { //if file is not existing
-    #endif
-        std::cout << "NOTE: creating export file" << std::endl; //status msg
-        #if defined _linux
-            mkdir(DFToutputPath.c_str(), 0);
-        #elif defined _WIN32 || _WIN64
-            mkdir(DFToutputPath.c_str());
-        #endif
-    }
     DFToutputFile = DFToutputPath + "DFToutput_" + getCurrentTime() + ".txt"; //filename with current time
 
     std::ofstream outputfile;
@@ -333,13 +532,4 @@ bool saveToFile(const std::vector< std::vector<float> >& outputList, const std::
     std::cout << std::endl;
     outputfile.close();
     return true;
-}
-
-const std::string getCurrentTime() {
-    time_t     now = time(0);
-    struct tm  tstruct;
-    char       buf[25];
-    tstruct = *localtime(&now);
-    strftime(buf, sizeof(buf), "%Y_%m_%d-%H_%M_%S", &tstruct);
-    return buf;
 }
